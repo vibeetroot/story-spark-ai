3e6c431a9e8ebd1dc2f1ac5df892cdaff4d462f7
nidhimaurya19 <nidhimaurya1909@gmail.com>
2026-05-31 13:30:39 +0530

    chore: remove unrelated layout changes

--- frontend/src/components/layout/root_layout.component.tsx
+++ frontend/src/components/layout/root_layout.component.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import { useLocation } from "react-router-dom";
-// import NavListComponent from "../hero/nav_list.component";
+import NavListComponent from "../hero/nav_list.component";
 import CookieConsentBanner from "../cookie-consent/cookie-consent.component";
 import FooterComponent from "../footer/footer.component";
 
@@ -21,8 +21,7 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
 
   return (
     <div className={`flex flex-col min-h-screen bg-slate-50 text-slate-900 transition-colors duration-300 dark:bg-slate-950 dark:text-slate-100 ${!isAuthPage ? "pb-20 lg:pb-0" : ""}`}>
-      {/* {!hideHeader && <NavListComponent />} */}
-      {!hideHeader && null}
+      {!hideHeader && <NavListComponent />}
       <CookieConsentBanner />
       <div className="flex-grow min-h-0">{children}</div>
       {!hideFooter && <FooterComponent />}
