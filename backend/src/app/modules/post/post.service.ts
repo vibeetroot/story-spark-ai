5713358c2459a8bf8deb8a06127c95da345d66ac
ritharna <ritharnapv@gmail.com>
2026-06-03 18:11:27 +0530

    fix(backend): resolve syntax and compile errors in post and reaction services

--- backend/src/app/modules/post/post.service.ts
+++ backend/src/app/modules/post/post.service.ts
@@ -16,12 +16,9 @@ import { SortOrder, Types } from "mongoose";
 import { GamificationService } from "../gamification/gamification.service";
 
 const MAX_SEARCH_TERM_LENGTH = 100;
-const escapeRegex = (text: string) => text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-
 const escapeRegex = (text: string): string => {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 };
-const MAX_SEARCH_TERM_LENGTH = 100;
 
 interface ICursorPayload {
   value: string;
@@ -407,13 +404,9 @@ const toggleBookmark = async (postId: string, token: ITokenPayload) => {
     throw new ApiError(httpStatus.BAD_REQUEST, "User not found!");
   }
 
-  const postExists = await Post.exists({ _id: postId, isDeleted: { $ne: true } });
-  if (!postExists) {
-
   const post = await Post.findOne({ _id: postId, isDeleted: { $ne: true } });
 
   if (!post) {
-
     throw new ApiError(httpStatus.BAD_REQUEST, "Post not found!");
   }
 
--- backend/src/app/modules/reaction/reaction.service.ts
+++ backend/src/app/modules/reaction/reaction.service.ts
@@ -30,13 +30,48 @@ const toggleReaction = async (
     throw new ApiError(httpStatus.BAD_REQUEST, "Post not found!");
   }
 
- main
+  const existingReaction = await Reaction.findOne({
+    postId: new Types.ObjectId(postId),
+    userId: user._id,
+    type: type,
+  });
+
+  if (existingReaction) {
+    await Reaction.deleteOne({ _id: existingReaction._id });
+
+    const updatedPost = await Post.findOneAndUpdate(
+      { _id: postId },
+      { $inc: { likesCount: -1 } },
+      { new: true }
+    );
+
+    if (updatedPost && updatedPost.likesCount < 0) {
+      await Post.updateOne(
+        { _id: postId },
+        { $set: { likesCount: 0 } }
+      );
+    }
+
+    return {
+      message: "Reaction removed",
+      likesCount: Math.max(0, updatedPost?.likesCount ?? 0),
+    };
+  } else {
     const newReaction = await Reaction.create({
       postId: new Types.ObjectId(postId),
       userId: user._id,
       type: type,
     });
- main
+
+    const updatedPost = await Post.findOneAndUpdate(
+      { _id: postId },
+      { $inc: { likesCount: 1 } },
+      { new: true }
+    );
+
+    return {
+      message: "Reaction added successfully",
+      likesCount: updatedPost?.likesCount || 0,
     };
   }
 };
